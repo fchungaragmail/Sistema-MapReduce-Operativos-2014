5a0c285f2dd6f55e1fc3f1a4f440502e175d338f
jclapadula <juan_lapa@hotmail.com>
2015-04-25 02:34:58 -0300

    Agrego el Ej 03 (Commons)

---
+++ Ejercicios/Juan/Ejercicio_03-Commons/src/Ejercicio_03-Commons.c
@@ -0,0 +1,119 @@
+/*
+ ============================================================================
+ Name        : Ejercicio_03-Commons.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <commons/string.h>
+#include <commons/txt.h>
+#include <commons/collections/list.h>
+#include "tipos.h"
+#include <commons/log.h>
+
+const int LINE_SIZE =200;
+
+void list(char*, t_list*);
+bool isGreater(t_persona*,t_persona*);
+void saveToFile(t_list*, FILE*);
+bool isMajor(t_persona*);
+
+int main(void) {
+	FILE* personasIN;
+	FILE* personasOUT;
+	int item;
+	char* line;
+	t_list* personasList = list_create();
+
+
+	personasIN = fopen("/home/utnso/workspace/Practica/Ejercicio_03-Commons/Personas.txt","r");
+    personasOUT = fopen("/home/utnso/workspace/Practica/Ejercicio_03-Commons/PersonasNuevo.txt","w");
+	if (personasIN == NULL)
+    {
+        printf("No se pudo abrir el archivo");
+    	exit(EXIT_FAILURE);
+    }
+    line = malloc(LINE_SIZE);
+
+
+
+	while (fgets(line,LINE_SIZE,personasIN)!=NULL)
+	{
+		list(line, personasList);
+	}
+
+	list_sort(personasList, (isGreater));
+	personasList = list_filter(personasList, (isMajor));
+
+	saveToFile(personasList, personasOUT);
+
+	fclose(personasIN);
+	fclose(personasOUT);
+	return EXIT_SUCCESS;
+}
+
+void list(char* line,t_list* lista)
+{
+	t_persona* persona = persona = malloc(sizeof(t_persona));
+	char** array;
+
+	array = string_split(line,";");
+	strcpy(persona->region,array[0]);
+	strcpy(persona->NomAp, array[1]);
+	sscanf(array[2],"%d",&(persona->edad));
+	sscanf(array[3],"%d",&(persona->Tel));
+	sscanf(array[4],"%d",&(persona->DNI));
+	free(array);
+
+	list_add(lista,persona);
+}
+
+bool isGreater(t_persona* persona1,t_persona* persona2)
+{
+	if (persona1->region == persona2->region)
+	{
+		if (persona1->edad >= persona2->edad)
+		{
+			return true;
+		}else
+		{
+			return false;
+		}
+	}else
+	{
+		if (persona1->region > persona2->region)
+		{
+			return true;
+		}else
+		{
+			return false;
+		}
+
+	}
+}
+
+
+void saveToFile(t_list* personasList,FILE* personasOUT)
+{
+	int i = 0;
+	for(i=0;i<personasList->elements_count;i++)
+	{
+		fprintf(personasOUT,"%s|%d|%d|%s|%d\n",
+				(((t_persona*)(list_get(personasList,i)))->region),
+				(((t_persona*)(list_get(personasList,i)))->edad),
+				(((t_persona*)(list_get(personasList,i)))->DNI),
+				(((t_persona*)(list_get(personasList,i)))->NomAp),
+				(((t_persona*)(list_get(personasList,i)))->Tel));
+	}
+}
+
+bool isMajor(t_persona* persona)
+{
+	return (persona->edad >= 18);
+}
---
+++ Ejercicios/Juan/Ejercicio_03-Commons/src/tipos.h
@@ -0,0 +1,20 @@
+/*
+ * Ejercicio_03-Commons.h
+ *
+ *  Created on: 19/4/2015
+ *      Author: utnso
+ */
+
+#ifndef TIPOS_H_
+#define TIPOS_H_
+
+typedef struct {
+	char* region[20];
+	int edad;
+	int DNI;
+	char NomAp[30];
+	int Tel;
+} t_persona;
+
+
+#endif /* TIPOS_H_ */
