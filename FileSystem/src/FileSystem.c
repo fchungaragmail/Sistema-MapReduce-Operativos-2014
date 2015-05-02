1051b5f408b5b928deb2b1329061497879142cf5
jclapadula <juan_lapa@hotmail.com>
2015-05-02 20:26:14 -0300

    Creo el proyecto FileSystem

---
+++ FileSystem/src/FileSystem.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : FileSystem.c
+ Author      : The ByteLess
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "estructuras.h"
+
+int main(void) {
+	puts(""); /* prints  */
+	return EXIT_SUCCESS;
+}
---
+++ FileSystem/src/estructuras.h
@@ -0,0 +1,13 @@
+/*
+ * estructuras.h
+ *
+ *  Created on: 2/5/2015
+ *      Author: utnso
+ */
+
+#ifndef ESTRUCTURAS_H_
+#define ESTRUCTURAS_H_
+
+
+
+#endif /* ESTRUCTURAS_H_ */
