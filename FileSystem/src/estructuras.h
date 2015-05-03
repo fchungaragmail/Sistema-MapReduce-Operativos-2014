38eaacf897563f86b5b46264f662eb70367dab02
jclapadula <juan_lapa@hotmail.com>
2015-05-02 21:11:05 -0300

    Primera definicion de las estructuras

--- FileSystem/src/estructuras.h
+++ FileSystem/src/estructuras.h
@@ -8,6 +8,23 @@
 #ifndef ESTRUCTURAS_H_
 #define ESTRUCTURAS_H_
 
+typedef enum {DISPONIBLE, NO_DISPONIBLE} t_estado;
 
+typedef struct {
+	char nodo[5];
+	unsigned int bloque;
+} t_ubicacion;
+
+typedef struct {
+	unsigned long long int tamanio;
+	unsigned short int dirPadre;
+	t_estado estado;
+	t_ubicacion** bloques;
+} t_reg_archivo;
+
+typedef struct {
+	char directorio[256];
+	int padre;
+} t_reg_directorio;
 
 #endif /* ESTRUCTURAS_H_ */
