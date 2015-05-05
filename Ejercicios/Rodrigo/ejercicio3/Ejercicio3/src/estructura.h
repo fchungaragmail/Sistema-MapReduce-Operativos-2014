a67201083fb593f3d341df5a619afa78ac787d17
rpetito <rpetito.93@gmail.com>
2015-05-05 02:06:57 -0300

    primera carga ejercicio3

---
+++ Ejercicios/Rodrigo/ejercicio3/Ejercicio3/src/estructura.h
@@ -0,0 +1,23 @@
+/*
+ * estructura.h
+ *
+ *  Created on: 4/5/2015
+ *      Author: utnso
+ */
+
+#ifndef ESTRUCT
+#define ESTRUCT
+
+typedef struct {
+
+	char region[20];
+	char apeNom[25];
+	int edad;
+	int DNI;
+	int numTel;
+	int credito;
+
+} t_persona;
+
+
+#endif
