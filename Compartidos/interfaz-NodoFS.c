bf038b19a797098cfe6007c8a970e0934a485eee
jclapadula <juan_lapa@hotmail.com>
2015-05-12 23:16:25 -0300

    Creados los archivos vacios para la interfaz Nodo-FileSystem. Tambien se aniadieron al proyecto del FS

---
+++ Compartidos/interfaz-NodoFS.c
@@ -0,0 +1,2 @@
+
+#include "interfaz-NodoFS.h"
---
+++ Compartidos/interfaz-NodoFS.h
@@ -0,0 +1,13 @@
+/*
+ * interfaz-NodoFS.h
+ *
+ *  Created on: 12/5/2015
+ *      Author: utnso
+ */
+
+#ifndef COMPARTIDOS_INTERFAZ_NODOFS_H_
+#define COMPARTIDOS_INTERFAZ_NODOFS_H_
+
+
+
+#endif /* COMPARTIDOS_INTERFAZ_NODOFS_H_ */
