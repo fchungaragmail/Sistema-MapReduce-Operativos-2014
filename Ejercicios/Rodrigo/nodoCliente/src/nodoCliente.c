9222676d9b88b1766898c66caa8572f0a13bcd52
rpetito <rpetito.93@gmail.com>
2015-05-21 00:20:28 -0300

    boceto basico cliente de nodo para prueba

---
+++ Ejercicios/Rodrigo/nodoCliente/src/nodoCliente.c
@@ -0,0 +1,41 @@
+/*
+ * nodoCliente.c
+ *
+ *  Created on: 21/5/2015
+ *      Author: utnso
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+
+#define PORT 8000
+
+int main() {
+
+
+	int socketCli;
+	struct sockaddr_in dir;
+	socklen_t dirsize;
+	char bufferRecive[200];
+
+	dir.sin_family = AF_INET;
+	dir.sin_port = htons(PORT);
+	dir.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	dirsize = sizeof(struct sockaddr);
+
+	socketCli = socket(AF_INET, SOCK_STREAM, 0);
+
+	connect(socketCli, &dir, &dirsize);
+
+	recv(socketCli, bufferRecive, 140, 0);
+
+
+
+
+	return 0;
+}
+
