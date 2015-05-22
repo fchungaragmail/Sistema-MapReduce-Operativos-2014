2bc7785fb5315dbdec627a8c709e5b3060d2ef90
rpetito <rpetito.93@gmail.com>
2015-05-22 01:55:12 -0300

    Agrego threads a nodo servidor

--- nodo/src/nodo.c
+++ nodo/src/nodo.c
@@ -9,17 +9,22 @@
 #include <stdio.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <stdlib.h>
 
 #define PORT 7500
 #define MSJ_LENGTH 256
 
+void* conectionHandler(void*);
 
 int main() {
 
-	int conectionListener;   //crear hilos, uno escucha al filesystem, el resto esperan map o reduce
+	int conectionListener;
 	int comunicationSocket;
-	char buffer[140];
+	int *auxSocket;
+	socklen_t length;
 	struct sockaddr_in sock_in, sock_out;
+
 
 	sock_in.sin_family = AF_INET;
 	sock_in.sin_port = htons(PORT);
@@ -30,39 +35,64 @@ int main() {
 	if (conectionListener == -1) {
 		printf("Fallo en creación de socket \n");
 		return -1;
+	} else {
+		printf("Socket creado con exito...\n");
 	}
 
-	memset(&conectionListener, '0', sizeof(conectionListener));  //limpio el contenido
-	memset(buffer, '0', sizeof(buffer));
 
-	bind(conectionListener, (struct sockaddr*) &sock_in, sizeof(sock_in));
+	if (bind(conectionListener, (struct sockaddr*) &sock_in, sizeof(struct sockaddr)) < 0) {
+		printf("No se pudo hacer Bind...\n");
+		return -1;
+	} else {
+		printf("Bind exitoso...\n");
+	}
 
 
 	//escucho conecciones entrantes, hasta 2 a la vez, verifico que sea menor o igual a 2 la cant de conecciones
 	if (listen(conectionListener, 2) == -1) {
 		printf("Sobrecarga del servidor \n");
 		return -1;
+	} else {
+		printf("Escuchando conecciones\n");
 	}
 
+	length = sizeof(struct sockaddr);
 
 	//acepto conecciones
-	while(1) {
+	comunicationSocket = accept(conectionListener, (struct sockaddr*) &sock_out, &length);
+	if (comunicationSocket != 1) {
+		printf("No se puede aceptar conección\n");
+	}
 
-	comunicationSocket = accept(conectionListener, (struct sockaddr*) &sock_out, NULL);
+	while (comunicationSocket) {
 
-	strcpy(buffer, "Hola, bienvenido al servidor que sirve");
-	send(comunicationSocket, buffer, sizeof(buffer), 0);
-	close(comunicationSocket);
+		printf("Conección aceptada\n");
+
+		pthread_t conectionThread;
+		auxSocket = malloc(sizeof(comunicationSocket));
+		*auxSocket = comunicationSocket;
+
+		pthread_create(&conectionThread, NULL, conectionHandler, (void*) &auxSocket );
+
+		pthread_join(conectionThread, NULL);
 
 	}
 
-
-
-
-
-
-
-
 	return 0;
+
+}
+
+
+void* conectionHandler(void* sockAux) {
+
+	int sock = * (int*) sockAux;
+	char* message;
+
+	message = "Hello client, I'm your server";
+
+	write(sock, message, sizeof(message));
+
+	close(sock);
+
 }
 
