950876c28ec2ad2b005ad67a6972e621b766bdfc
jclapadula <jclapadula@lapasoft.com>
2015-05-22 23:06:49 -0300

    Agregado el archivo de interfaz MaRTA/FileSystem

---
+++ Compartidos/interfaz-MartaFS.h
@@ -0,0 +1,20 @@
+/*
+ * interfaz-MartaFS.h
+ *
+ *  Created on: 22/5/2015
+ *      Author: utnso
+ */
+
+#ifndef COMPARTIDOS_INTERFAZ_MARTAFS_H_
+#define COMPARTIDOS_INTERFAZ_MARTAFS_H_
+
+
+//meter en este archivo todas las posibles interacciones entre
+//MaRTA y el FileSystem.
+
+//FileSystem ya permite leer los comandos pedidos en la consigna.
+//Faltarian definir los posibles parametros...
+
+
+
+#endif /* COMPARTIDOS_INTERFAZ_MARTAFS_H_ */
