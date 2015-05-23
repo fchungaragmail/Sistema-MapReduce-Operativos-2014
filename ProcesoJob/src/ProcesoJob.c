6f63e8f21dec214e868b85b10bf8eda91326eb52
William <william.herrera89@gmail.com>
2015-05-22 22:46:29 -0300

    - ProcesoJob: agregado esqueleto para el manejo de los hilosMapper

--- ProcesoJob/src/ProcesoJob.c
+++ ProcesoJob/src/ProcesoJob.c
@@ -5,17 +5,32 @@
 #include <arpa/inet.h>
 #include <commons/config.h>
 
+
 int socketFd = -1;
-t_list* listaConexiones;
+t_list* listaHilos;
 pthread_t threadMartaHandler;
 t_config* configuracion;
 
 
 void* martaHandler(void* arg){
 
-	while(1){
+	listaHilos = list_create();
 
+	while(TRUE){
+		//TODO - recv(PeticionMartaJob)
+		//SOLO TEST
+
+		HiloJob* hiloJob = malloc(sizeof(HiloJob));
+		hiloJob->direccionNodo = NULL;
+		hiloJob->estadoHilo = ESTADO_HILO_NUEVO;
+		hiloJob->threadhilo = NULL;
+
+		list_add(listaHilos,(void*)hiloJob);
+		CrearHiloMapper(hiloJob);
+		break;
+		/////
 	}
+	list_destroy(listaHilos);
 }
 
 void IniciarConfiguracion(){
@@ -56,17 +71,19 @@ void IniciarConexionMarta(){
 	their_addr.sin_port = htons(ipMarta);
 	inet_aton(puertoMarta, &(their_addr.sin_addr));
 	memset(&(their_addr.sin_zero), '\o', 8);
-
+	//COMENTADO PARA TEST
+/*
 	if (connect(socketFd, (Sockaddr_in*) &their_addr, sizeof(Sockaddr_in))
 			== -1) {
 		error_show("Error al conectarse con MARTA\n");
 		Terminar(EXIT_ERROR);
 	}
-
+*/
+	/////
 	printf("Conexion exitosa con Marta, ip : %s, puerto :%s\n",ipMarta,puertoMarta);
 	pthread_create(&threadMartaHandler, NULL, martaHandler, NULL);
+	pthread_join(threadMartaHandler,NULL);
 }
-
 
 int main(int argc,char* argv[] ){
 
