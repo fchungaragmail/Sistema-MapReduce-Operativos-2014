75ec2faf1f5f1a20cdec3863172ced00371c4946
jclapadula <jclapadula@lapasoft.com>
2015-05-23 02:03:22 -0300

    Intercambios entre Nodo/FS a desarrollar

--- Compartidos/interfaz-NodoFS.h
+++ Compartidos/interfaz-NodoFS.h
@@ -8,6 +8,18 @@
 #ifndef COMPARTIDOS_INTERFAZ_NODOFS_H_
 #define COMPARTIDOS_INTERFAZ_NODOFS_H_
 
+//El FileSystem u otro proceso Nodo podrán solicitar las siguientes operaciones:
 
+//- getBloque(numero) ​
+//:   Devolverá   el   contenido   del   bloque   solicitado   almacenado   en   el
+//Espacio de Datos.
+
+//- setBloque(numero,   [datos]) ​
+//:   Grabará   los   datos   enviados   en   el   bloque   solicitado   del
+//Espacio de Datos
+
+// getFileContent(nombre) ​
+//:   Devolverá   el   contenido   del   archivo   de   Espacio   Temporal
+//solicitado.
 
 #endif /* COMPARTIDOS_INTERFAZ_NODOFS_H_ */
