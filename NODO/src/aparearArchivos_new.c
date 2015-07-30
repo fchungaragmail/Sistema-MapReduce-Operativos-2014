9796f41a2507fa5d4b3244dadc0da2c08329872d
Gigi <giselleariadna@gmail.com>
2015-07-30 19:49:58 -0300

    wait pid

--- NODO/src/aparearArchivos_new.c
+++ NODO/src/aparearArchivos_new.c
@@ -29,6 +29,7 @@ char* aparearArchivos(char* listaArchivos) {
 		char* tempFile = string_new();
 		string_append_with_format(&tempFile, "/tmp%s", archivo[i]);
 		int archivoFD = open(tempFile, O_RDONLY);
+
 		struct stat infoArchivo;
 		stat(tempFile, &infoArchivo);
 		char *contenido = malloc(infoArchivo.st_size);
@@ -45,10 +46,13 @@ char* aparearArchivos(char* listaArchivos) {
 	close(apareoArchivo);
 
 
-	if(fork() == 0) {
+	int resultFork;
+	resultFork = fork();
 
-	    close(0); //cierro entrada estandar, queda libre ese fd y el proximo fd se asociara a la entrada estandar
-	    open(path, O_RDONLY); //proximo fd, se asocia a la entrada estandar
+	if(resultFork == 0) {
+
+		close(0); //cierro entrada estandar, queda libre ese fd y el proximo fd se asociara a la entrada estandar
+		open(path, O_RDONLY); //proximo fd, se asocia a la entrada estandar
 
 		close(1); //cierro salida estandar, el fd queda libre, proximo fd se asociara a stdout
 		creat(apareoOrdenado, 0777); //fd que se asocia a salida standard
@@ -58,7 +62,13 @@ char* aparearArchivos(char* listaArchivos) {
 		exit(EXIT_SUCCESS);//se aplica sort a lo que hay en stdin (fd del archivo) y sale por stdout (fd archivo ordenado)
 	}
 
-	wait(0);
+	int status;
+	waitpid(resultFork, &status, 0);
+	if(WIFEXITED(status)) {
+		log_info(log_nodo, "Child sort apareo termino bien");
+	} else {
+		log_info(log_nodo, "Child sort apareo termino MAL");
+	}
 
 	unlink(path);
 
